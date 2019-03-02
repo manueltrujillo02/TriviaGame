56d657d5df4ca353ef493d16c8b1e3d3a75d2d16
Bat <noreply>
2019-03-02 14:42:03 -0800

    homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,50 +4,88 @@
   var correct = 0;
   var wrong = 0;
   var numOfQuestions = 3;
-  var ansArr = ["a","b","c"]
+  var ansArr = ["a", "f", "k"]
   var intervalId;
-  var q1 = document.forms["riddle"]["q1"].value;
-  var q2 = document.forms["riddle"]["q2"].value;
-  var q3 = document.forms["riddle"]["q3"].value;
-
-  function decrement() {
-    number--;
-    $("#show-number").html("<h1>" + number + "</h1>");
-  }
-
-  // function stop() {
-  //   clearInterval(intervalId);
-  // }
-
-
-  // if (number === 0) {
-  //   stop();
-  // }
-
-  $(".start").on("click", function() {
+  
+  
+  
+  function stop() {
     clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  });
-
-
-
-function onSubmit(){
-  event.preventDefault();
-  
-
-  for(var i = 1; i <= numOfQuestions; i++){
-    if(q1 === ansArr[i - 1]) {
-      correct++
-      $("#correct").text("CORRECT ANSWERS: " + correct)
-      
-      console.log(correct);
-    };
   }
-  // stop();
-}
-
-
+  clearInterval(intervalId);
   
-
-
-
+    
+  
+  $("#the-start").on("click", function () {
+    intervalId = setInterval(decrement, 1000);
+     function decrement() {
+      number--;
+      $("#show-number").html("<h1>" + number + "</h1>");
+      if (number === 0) {
+        
+        
+        var q1 = document.forms["riddle"]["q1"].value;
+        var q2 = document.forms["riddle"]["q2"].value;
+        var q3 = document.forms["riddle"]["q3"].value;
+        for (var i = 1; i <= numOfQuestions; i++) {
+          if (q1 === ansArr[i - 1]) {
+              correct++
+              $("#correct").text("CORRECT ANSWERS: " + correct)
+          };
+      
+          if (q2 === ansArr[i]) {
+              correct++
+              $("#correct").text("CORRECT ANSWERS: " + correct)
+          };
+          if (q3 === ansArr[i + 1]) {
+              correct++
+              $("#correct").text("CORRECT ANSWERS: " + correct)
+      
+      
+              stop();
+              console.log(correct);
+              console.log(wrong)
+          }
+          
+            stop();
+        }
+       
+      
+      };
+    };
+  
+  });
+  
+  
+  
+  $(".submit").on("click", function () {
+    // function onSubmit(){
+    event.preventDefault();
+    var q1 = document.forms["riddle"]["q1"].value;
+    var q2 = document.forms["riddle"]["q2"].value;
+    var q3 = document.forms["riddle"]["q3"].value;
+    for (var i = 1; i <= numOfQuestions; i++) {
+      if (q1 === ansArr[i - 1]) {
+          correct++
+          $("#correct").text("CORRECT ANSWERS: " + correct)
+      };
+  
+      if (q2 === ansArr[i]) {
+          correct++
+          $("#correct").text("CORRECT ANSWERS: " + correct)
+      };
+      if (q3 === ansArr[i + 1]) {
+          correct++
+          $("#correct").text("CORRECT ANSWERS: " + correct)
+  
+          stop();
+          console.log(correct);
+          console.log(wrong)
+      }
+      
+        stop();
+    }
+  
+  
+  });
+  
